0251c4ce7cbe218570053bf54a7f69657716f830
Noah Huppert <3323504+Noah-Huppert@users.noreply.github.com>
2024-05-19 02:37:10 -0400

    emby: added get user media items call

--- server/emby/client.go
+++ server/emby/client.go
@@ -2,6 +2,8 @@ package emby
 
 import (
 	"fmt"
+	"golang.org/x/net/context"
+	"strings"
 
 	"github.com/Noah-Huppert/media-recyclarr/restclient"
 )
@@ -36,4 +38,71 @@ func NewEmbyClient(opts NewEmbyClientOpts) (*EmbyClient, error) {
 	return &EmbyClient{
 		apiClient: apiClient,
 	}, nil
+}
+
+// PaginatableResponse is the format in which the Emby API will return a page of results
+type PaginatableResponse[Item interface{}] struct {
+	// Items returned for page
+	Items []Item `validate:"required"`
+
+	// TotalRecordCount is the total number of items available
+	TotalRecordCount int `validate:"required"`
+}
+
+// MediaItem contains details about a piece of media in the server
+type MediaItem struct {
+	// ID is the unique ID of the media item in the server
+	ID string `json:"Id" validate:"required"`
+
+	// Name is the name of the media item
+	Name string `validate:"required"`
+
+	// IndexNumber is the episode number (starting at 1) if the media item is part of a series
+	IndexNumber *int
+
+	// ParentIndexNumber is the season number (starting at 1) if the media item is part of a series
+	ParentIndexNumber *int
+
+	// IsFolder indicates if the media item is actually made up multiple child media items (ex., a TV series or season)
+	IsFolder bool `validate:"required"`
+
+	// Type of media
+	Type string `validate:"required oneof=Episode,Season,Movie"`
+}
+
+// UserMediaItem is a media item with details specific to a user
+type UserMediaItem struct {
+	MediaItem
+
+	// UserData is user specific information about the media item
+	UserData struct {
+		// Played indicates if the user has watched the media item
+		Played bool `validate:"required"`
+	} `validate:"required"`
+}
+
+func (client *EmbyClient) GetUserMediaItems(ctx context.Context, userID string, ids []string) ([]UserMediaItem, error) {
+	if len(ids) == 0 {
+		return nil, fmt.Errorf("ids cannot be empty")
+	}
+
+	var resp PaginatableResponse[UserMediaItem]
+	err := client.apiClient.MakeRequest(restclient.MakeRequestOpts{
+		Ctx:    ctx,
+		Method: "GET",
+		Path:   fmt.Sprintf("/emby/Users/%s/items", userID),
+		QueryParams: map[string]string{
+			"ids": strings.Join(ids, ","),
+		},
+		Resp: &resp,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to make request: %s", err)
+	}
+
+	if resp.TotalRecordCount != len(resp.Items) {
+		return nil, fmt.Errorf("no pagination configured but results do not contain all items")
+	}
+
+	return resp.Items, nil
 }
