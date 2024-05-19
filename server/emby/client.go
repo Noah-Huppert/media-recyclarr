1d8a42bc855539f6ffd97c89022da56257c14c08
Noah Huppert <contact@noahh.io>
2024-05-19 17:19:26 -0400

    wrote logic to get tree of media

--- server/emby/client.go
+++ server/emby/client.go
@@ -58,6 +58,13 @@ func (resp PaginatableResponse[Item]) CheckItemsCount() error {
 	return nil
 }
 
+const (
+	MediaItemTypeSeries  = "Series"
+	MediaItemTypeSeason  = "Season"
+	MediaItemTypeEpisode = "Episode"
+	MediaItemTypeMovie   = "Movie"
+)
+
 // MediaItem contains details about a piece of media in the server
 type MediaItem struct {
 	// ID is the unique ID of the media item in the server
@@ -75,25 +82,45 @@ type MediaItem struct {
 	// IsFolder indicates if the media item is actually made up multiple child media items (ex., a TV series or season)
 	IsFolder bool `validate:"required"`
 
+	// SeriesID is the ID of the parent series if the media item is part of a series
+	SeriesID *string `json:"SeriesId"`
+
 	// Type of media
-	Type string `validate:"required oneof=Episode,Season,Movie"`
+	Type string `validate:"required oneof=Series,Season,Episode,Movie"`
 }
 
-// GetMediaItems retries media items
-func (client *EmbyClient) GetMediaItems(ctx context.Context, ids []string) ([]MediaItem, error) {
-	if len(ids) == 0 {
-		return nil, fmt.Errorf("ids cannot be empty")
+// ListMediaItemsOpts are query options for ListMediaItems
+type ListMediaItemsOpts struct {
+	// Recursive indicates items should be recursively searched for
+	Recursive *bool
+
+	// IncludeItemTypes are media item types to include in results
+	IncludeItemTypes []string
+
+	// IDs of media items to return
+	IDs []string
+}
+
+// ListMediaItems retrieves media items
+func (client *EmbyClient) ListMediaItems(ctx context.Context, opts ListMediaItemsOpts) ([]MediaItem, error) {
+	queryParams := map[string]string{}
+	if opts.Recursive != nil {
+		queryParams["Recursive"] = fmt.Sprint(opts.Recursive)
+	}
+	if opts.IncludeItemTypes != nil {
+		queryParams["IncludeItemTypes"] = strings.Join(opts.IncludeItemTypes, ",")
+	}
+	if opts.IDs != nil {
+		queryParams["Ids"] = strings.Join(opts.IDs, ",")
 	}
 
 	var resp PaginatableResponse[MediaItem]
 	err := client.apiClient.MakeRequest(restclient.MakeRequestOpts{
-		Ctx:    ctx,
-		Method: "GET",
-		Path:   "/emby/Items",
-		QueryParams: map[string]string{
-			"Ids": strings.Join(ids, ","),
-		},
-		Resp: &resp,
+		Ctx:         ctx,
+		Method:      "GET",
+		Path:        "/emby/Items",
+		QueryParams: queryParams,
+		Resp:        &resp,
 	})
 	if err != nil {
 		return nil, fmt.Errorf("failed to make request: %s", err)
@@ -117,7 +144,8 @@ type UserMediaItem struct {
 	} `validate:"required"`
 }
 
-func (client *EmbyClient) GetUserMediaItems(ctx context.Context, userID string, ids []string) ([]UserMediaItem, error) {
+// ListUserMediaItems retrieves user details about media items
+func (client *EmbyClient) ListUserMediaItems(ctx context.Context, userID string, ids []string) ([]UserMediaItem, error) {
 	if len(ids) == 0 {
 		return nil, fmt.Errorf("ids cannot be empty")
 	}
@@ -179,14 +207,14 @@ func (client *EmbyClient) ListUsers(ctx context.Context) ([]User, error) {
 	return resp.Items, nil
 }
 
-// ListShowEpisodes retrieves episodes for a TV show
-func (client *EmbyClient) ListShowEpisodes(ctx context.Context, showID string) ([]MediaItem, error) {
+// ListShowSeasons retrieves seasons for a TV show
+func (client *EmbyClient) ListShowSeasons(ctx context.Context, showID string) ([]MediaItem, error) {
 	var resp PaginatableResponse[MediaItem]
 
 	err := client.apiClient.MakeRequest(restclient.MakeRequestOpts{
 		Ctx:    ctx,
 		Method: "GET",
-		Path:   fmt.Sprintf("/emby/Shows/%d/Episodes", showID),
+		Path:   fmt.Sprintf("/emby/Shows/%d/Seasons", showID),
 		Resp:   &resp,
 	})
 	if err != nil {
@@ -198,4 +226,117 @@ func (client *EmbyClient) ListShowEpisodes(ctx context.Context, showID string) (
 	}
 
 	return resp.Items, nil
+}
+
+// ListShowEpisodes retrieves episodes for a TV show
+func (client *EmbyClient) ListShowEpisodes(ctx context.Context, showID string, seasonID string) ([]MediaItem, error) {
+	var resp PaginatableResponse[MediaItem]
+
+	err := client.apiClient.MakeRequest(restclient.MakeRequestOpts{
+		Ctx:    ctx,
+		Method: "GET",
+		Path:   fmt.Sprintf("/emby/Shows/%d/Episodes", showID),
+		QueryParams: map[string]string{
+			"SeasonId": seasonID,
+		},
+		Resp: &resp,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to make request: %s", err)
+	}
+
+	if err := resp.CheckItemsCount(); err != nil {
+		return nil, err
+	}
+
+	return resp.Items, nil
+}
+
+// MediaItemNode is a media item which can hold child media items
+type MediaItemNode struct {
+	MediaItem
+
+	// Children are media items which belong to the parent media item, like episodes of a season
+	Children []MediaItemNode
+}
+
+// NewMediaItemNode makes a new MediaItemNode from a MediaItem
+func NewMediaItemNode(item MediaItem) MediaItemNode {
+	return MediaItemNode{
+		MediaItem: item,
+		Children:  []MediaItemNode{},
+	}
+}
+
+// NewMediaItemNodeArray makes a new array of MediaItemNode from an array of MediaItem
+func NewMediaItemNodeArray(items []MediaItem) []MediaItemNode {
+	nodes := []MediaItemNode{}
+
+	for _, one_item := range items {
+		nodes = append(nodes, NewMediaItemNode(one_item))
+	}
+
+	return nodes
+}
+
+// GetMediaTree recursively retrieves all the media items and their children
+// Optionally parent can be specified to start building the tree with the children of the specified media item
+func (client *EmbyClient) GetMediaTree(ctx context.Context, parent *MediaItemNode) ([]MediaItemNode, error) {
+	children := []MediaItemNode{}
+
+	// Either get all media items or start with parent
+	if parent == nil {
+		// Get all movies and series
+		trueVal := true
+
+		items, err := client.ListMediaItems(ctx, ListMediaItemsOpts{
+			Recursive: &trueVal,
+			IncludeItemTypes: []string{
+				MediaItemTypeMovie,
+				MediaItemTypeSeries,
+			},
+		})
+		if err != nil {
+			return nil, fmt.Errorf("failed to list movies and series: %s", err)
+		}
+
+		children = NewMediaItemNodeArray(items)
+	} else {
+		// Get children of specific type of parent
+		if parent.Type == MediaItemTypeSeries {
+			// Get seasons of parent series
+			seasons, err := client.ListShowSeasons(ctx, parent.ID)
+			if err != nil {
+				return nil, fmt.Errorf("failed to list seasons for series: %s", err)
+			}
+
+			children = NewMediaItemNodeArray(seasons)
+		} else if parent.Type == MediaItemTypeSeason {
+			// Get episodes of parent season
+			if parent.SeriesID == nil {
+				return nil, fmt.Errorf("cannot get episodes of season if SeriesID is nil")
+			}
+
+			episodes, err := client.ListShowEpisodes(ctx, parent.ID, *parent.SeriesID)
+			if err != nil {
+				return nil, fmt.Errorf("failed to list episodes for season: %s", err)
+			}
+
+			children = NewMediaItemNodeArray(episodes)
+		}
+
+		// No other media type has children
+	}
+
+	// Recursively get children of each child
+	for childIdx, child := range children {
+		subChildren, err := client.GetMediaTree(ctx, &child)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get children of media item ID '%s': %s", child.ID, err)
+		}
+
+		children[childIdx].Children = subChildren
+	}
+
+	return children, nil
 }
